6969cbb563cb89fec4d387b3209bd9b1efc4b035
mattyou736 <36690480+mattyou736@users.noreply.github.com>
2018-11-12 14:20:06 +0100

    start to the prototype

---
+++ Unity/Smup-game/Assets/Scripts/Player/Bullet.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour {
+
+    private float speed;
+    private Rigidbody RigidBullet;
+
+    // Use this for initialization
+    void Start ()
+    {
+        speed = 10;
+        RigidBullet = GetComponent<Rigidbody>();
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        RigidBullet.velocity = transform.up * speed;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(gameObject);
+    }
+}
