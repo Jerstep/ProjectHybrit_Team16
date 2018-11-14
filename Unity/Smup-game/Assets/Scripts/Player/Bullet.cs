f6e09036accdd26c0e0df1ef4f588cf8c56317a5
mattyou736 <36690480+mattyou736@users.noreply.github.com>
2018-11-14 14:16:45 +0100

    prototype

---
+++ Unity/Smup-game/Assets/Scripts/Player/Bullet.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour {
+
+    private float speed;
+    private Rigidbody RigidBullet;
+    private UIManager uiManager;
+
+    public bool heavy;
+    // Use this for initialization
+    void Start ()
+    {
+        if (!heavy)
+        {
+            speed = 10;
+        }
+        else
+        {
+            speed = 5;
+        }
+        RigidBullet = GetComponent<Rigidbody>();
+        uiManager = GameObject.Find("Canvas").GetComponent<UIManager>();
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        RigidBullet.velocity = transform.up * speed;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        
+        if(other.tag == "Enemy")
+        {
+            uiManager.score += 10;
+            Destroy(other.gameObject);
+            if (!heavy)
+            {
+                Destroy(gameObject);
+            }              
+        }
+        if (other.tag == "WallDeath")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Unity/Smup-game/Assets/Scripts/Player/Player.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    //rigidbody
+    private Rigidbody RigidPlayer;
+    private static bool playerExists;
+
+    public float moveSpeed;
+    public GameObject bulletPrefab , heavyBulletPrefab;
+    public Transform firePoint;
+
+    // Use this for initialization
+    void Start ()
+    {
+        RigidPlayer = GetComponent<Rigidbody>();
+        if (!playerExists)
+        {
+            playerExists = true;
+            DontDestroyOnLoad(transform.gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //left right
+        if (Input.GetAxisRaw("Horizontal") > 0.5f || Input.GetAxisRaw("Horizontal") < -0.5f)
+        {
+            RigidPlayer.velocity = new Vector3(Input.GetAxisRaw("Horizontal") * moveSpeed, RigidPlayer.velocity.y,0);
+        }
+        //up down
+        if (Input.GetAxisRaw("Vertical") > 0.5f || Input.GetAxisRaw("Vertical") < -0.5f)
+        {
+            //transform.Translate(new Vector3(0, Input.GetAxisRaw("Vertical")* moveSpeed * Time.deltaTime , 0));
+            RigidPlayer.velocity = new Vector3(RigidPlayer.velocity.x, Input.GetAxisRaw("Vertical") * moveSpeed, 0);
+        }
+
+        //stp moving
+        if (Input.GetAxisRaw("Horizontal") < 0.5f && Input.GetAxisRaw("Horizontal") > -0.5f)
+        {
+            RigidPlayer.velocity = new Vector3(0f, RigidPlayer.velocity.y, 0);
+        }
+        if (Input.GetAxisRaw("Vertical") < 0.5f && Input.GetAxisRaw("Vertical") > -0.5f)
+        {
+            RigidPlayer.velocity = new Vector3(RigidPlayer.velocity.x, 0f, 0);
+        }
+
+        if(Input.GetKeyDown("space"))
+        {
+            Instantiate(bulletPrefab, firePoint.position, Quaternion.identity);
+        }
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            Instantiate(heavyBulletPrefab, firePoint.position, Quaternion.identity);
+        }
+    }
+}
