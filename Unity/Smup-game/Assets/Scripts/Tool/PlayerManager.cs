813e60d54eab1866d806df400a0a38f909cfe2cd
Unknown <jeremy.koskamp@live.nl>
2018-11-26 01:22:54 +0100

    [+] start tool

---
+++ Unity/Smup-game/Assets/Scripts/Tool/PlayerManager.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour {
+
+    public GameObject playerOne;
+    public GameObject playerTwo;
+
+    // Use this for initialization
+    void Start () {		
+	}
+}
