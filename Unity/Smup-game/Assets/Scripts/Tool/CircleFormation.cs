513f9a10061351d41b79b27d12128cde905ef11e
Unknown <jeremy.koskamp@live.nl>
2018-12-02 14:06:17 +0100

    [/] Changed Player script

--- Unity/Smup-game/Assets/Scripts/Tool/CircleFormation.cs
+++ Unity/Smup-game/Assets/Scripts/Tool/CircleFormation.cs
@@ -26,7 +26,7 @@ public class CircleFormation : MonoBehaviour {
         }
 
         ////// the core bit ///////
-        Quaternion quaternion = Quaternion.AngleAxis(360f / (float)(pointCount - 1), transform.up);
+        Quaternion quaternion = Quaternion.AngleAxis(360f / (float)(pointCount), transform.up);
         Vector3 vec3 = transform.forward * radius;
         for(int index = 0; index < pointCount; ++index)
         {
@@ -45,6 +45,7 @@ public class CircleFormation : MonoBehaviour {
             {
                 // doesn't matter, we're updating the positions later
                 markers.Add(Instantiate(markerPrefab, Vector3.zero, Quaternion.identity) as Transform);
+                markers[i].transform.parent = gameObject.transform;
             }
         }
         if(pointCount < markers.Count)
