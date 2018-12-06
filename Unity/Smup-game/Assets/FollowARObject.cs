7efe1fb2426aadf42eeaf9e0de4213ee357133dd
Unknown <jeremy.koskamp@live.nl>
2018-12-06 11:39:24 +0100

    Auto stash before merge of "Jeremy" and "origin/Matt"

---
+++ Unity/Smup-game/Assets/FollowARObject.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowARObject : MonoBehaviour {
+
+    private Transform ARTarget;
+
+	// Use this for initialization
+	void Start () {
+        if(ARTarget == null)
+        {
+            ARTarget = GameObject.FindGameObjectWithTag("ARTarget").transform;
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if(ARTarget != null)
+        {
+            transform.position = new Vector3(ARTarget.position.x, ARTarget.position.y, ARTarget.position.z);
+            transform.rotation = ARTarget.transform.rotation;
+        }
+        else
+        {
+            transform.position = this.transform.position;
+        }
+	}
+}
